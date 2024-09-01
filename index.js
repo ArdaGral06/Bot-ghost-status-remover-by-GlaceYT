1eb995129e326f125473cb1eacd1cc8bd382a816
ArdaGral06 <ardaeser2432@gmail.com>
2024-09-01 20:31:32 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to Spotify","Playing VALORANT","ROBLOX oynuyor"];
 
 
 let currentIndex = 0;
